5a1bff794b23c035df713cbc3dc2670b064a3508
Tony <tony19970104@gmail.com>
2025-08-05 23:41:05 +0800

    feat: support dataset_path

--- train.py
+++ train.py
@@ -1,36 +1,40 @@
 from hydra import compose, initialize
 from lightning import Trainer
-from allinvision_detection.yolo.tools.solver import TrainModel
+from yolo.tools.solver import TrainModel
+from typing import Optional
+import yaml
 
 class DetectionTrainer:
     def __init__(
         self,
-        config_path="yolo/config",
-        config_name="config",
-        model_name="v9-s",
-        dataset_name=None,
-        class_num=4,
-        batch_size=None,
-        epochs=100,
+        dataset_path: Optional[str] = None,
+        config_path: Optional[str] = "yolo/config",
+        config_name: Optional[str] = "config",
+        model_name: Optional[str] = "v9-s",
+        batch_size: Optional[int] = 16,
+        epochs: Optional[int] = 100,
     ):
         with initialize(config_path=config_path, version_base=None, job_name="train_job"):
             overrides = [
                 "task=train",
                 f"model={model_name}",
-                f"dataset.class_num={class_num}",
             ]
-            if dataset_name:
-                overrides.append(f"dataset={dataset_name}")
 
             cfg = compose(
                 config_name=config_name,
                 overrides=overrides
             )
-            if batch_size:
-                cfg.task.data.batch_size = batch_size
-            
+
+            if dataset_path:
+                with open(dataset_path, 'r') as f:
+                    user_dataset_dict = yaml.safe_load(f)
+
+                if user_dataset_dict:
+                    for key, value in user_dataset_dict.items():
+                        cfg.dataset[key] = value
+
+            cfg.task.data.batch_size = batch_size
             cfg.task.epoch = epochs
-            
             self.cfg = cfg
 
         self.model = TrainModel(self.cfg)
@@ -55,9 +59,9 @@ if __name__ == "__main__":
         config_path="yolo/config",
         config_name="config",
         model_name="v9-t",
-        dataset_name="africanwildlife",
-        class_num=4,
-        batch_size=32,
+        dataset_path="C:/Users/tsung-hung/Downloads/YOLO/africanwildlife.yaml",
+        batch_size=16,
         epochs=10,
     )
-    trainer.train()
+    print(trainer.cfg)
+    # trainer.train()
